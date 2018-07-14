4a88d730ad520545417431f3dd61fe9faf926f32
anders2D <35197477+anders2D@users.noreply.github.com>
2018-07-13 21:56:26 -0500

    Fix conectados hoy

--- core/querys.py
+++ core/querys.py
@@ -1,26 +1,15 @@
 # Copyright (c) 2017-2018 ferchoafta@gmail.com
 # Distribuido bajo la licencia MIT Software Licence
 # Mas informacion http://www.opensource.org/licenses/mit-license.php
-event_top="""CREATE event IF NOT EXISTS actualizar_top ON schedule every 1 minute starts "2018-01-01 00:00:00" do 
+event_top="""CREATE event IF NOT EXISTS actualizar_top ON schedule every 1 hour starts "2018-01-01 00:00:00" do 
 BEGIN 
-INSERT INTO estadisticas_registroconectados 
-              ( 
-
-                          time, 
-                          count, 
-
-              ) 
-  SELECT id, 
-         account_id, 
-         NAME, 
-         exp, 
-         0,0 
-  FROM   player.player 
-  WHERE  id> 
-         ( 
-                SELECT ifnull(max(id), 0) 
-                FROM   varios_top); 
-				
+	INSERT INTO estadisticas_registroconectados 
+				(TIME, 
+				 count) 
+	VALUES      (Now(), 
+				 (SELECT Count(*) 
+				  FROM   player.player 
+				  WHERE  last_play > Date_sub(Now(), interval 55 minute)) ) ;
   INSERT INTO varios_top 
               ( 
                           id, 
@@ -65,5 +54,5 @@ end ;"""
 
 event_top=event_top.replace("\n","")
 event_top=event_top.replace("\r","")
-# event_top=event_top.replace("    ","")
-print (event_top)
+event_top=event_top.replace("      ","")
+# print (event_top)
